1652ba0732bd558fe26f80c8f41860cfa4192bca
Blaster1111 <rudrashukla0711@gmail.com>
2025-02-13 11:16:49 +0530

    server.js modified

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js';
+import mediaRoutes from "./routes/media.route.js";
 
 dotenv.config();
 connectDb();
@@ -10,7 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api", )
+
+app.use("/api", mediaRoutes);
+
 app.get("/",(req,res)=>{
     res.send("Clinikk Intern Task Runnig");
 })
